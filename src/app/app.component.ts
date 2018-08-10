35447be9f34dc47b2b1a8a7ec8e62f0dc288bace
denukaNirmalee <nirdima@gmail.com>
2018-08-10 12:19:30 +0530

    first commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+import { Observable, Subject } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularJsonProject';
+  title = 'app works!';
+  private apiUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
+  data: any = {};
+
+  constructor(private http: Http){
+    console.log('Hello fellow user');
+     this.getContacts();
+     this.getData();
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl).map((res: Response) => res.json())
+  }
+
+  //data is where we store response from the API
+  //contacts is what the API has called the arrays.
+  // Then we assign all attributes (that the API has) to the contact
+
+  getContacts(){
+    this.getData().subscribe(data =>{
+      console.log(data);
+      this.data = data
+    })
+  }
 }
